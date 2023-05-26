a3c8d60b59e8417402e7e188d2fa7c03ea91a5e0
Fernando Ferreira Passe <fernandopasse@hotmail.com>
2023-05-25 21:23:59 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import appConfig from './config/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CoreModule } from './core/core.module';
 
@@ -19,5 +21,7 @@ import { CoreModule } from './core/core.module';
     CoreModule,
     ProductsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
